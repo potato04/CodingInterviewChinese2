d016f99eec6850c25a6b8dc92ec0fa68f38c5e4e
shiwangwang <shiww@outlook.com>
2018-09-01 23:27:12 +0800

    complete 56

---
+++ 56_01_NumbersAppearOnce.playground/Contents.swift
@@ -0,0 +1,99 @@
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+//==================================================================
+
+// 面试题56（一）：数组中只出现一次的两个数字
+// 题目：一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序
+// 找出这两个只出现一次的数字。要求时间复杂度是O(n)，空间复杂度是O(1)。
+
+import Foundation
+import XCTest
+
+class Solution {
+    
+    /**
+    返回数组中只出现一次的两个数字
+     - parameters:
+        - nums: 数组
+     - Returns: 两个数字
+     */
+    func FindNumsAppearOnce(_ nums: [Int]) -> (num1: Int?, num2: Int?) {
+        guard nums.count >= 2 else {
+            return (nil, nil)
+        }
+        let xorResult = nums.reduce(0) {
+            return $0 ^ $1
+        }
+        let indexOf1 = FindFirstBitIs1(xorResult)
+        var num1 = 0, num2 = 0
+        nums.forEach {
+            if(IsBit1($0, at: indexOf1)) {
+                num1 ^= $0
+            } else {
+                num2 ^= $0
+            }
+        }
+        return (min(num1, num2), max(num1, num2))
+    }
+    /**
+     查找从右边数起第一个是1的位
+     - parameters:
+     - num: 数字
+     - index: 第index位（从右开始）
+     - Returns: 判断结果
+     */
+    private func FindFirstBitIs1(_ num: Int) -> Int{
+        var index = 0
+        var num = num
+        while num & 1 == 0 && index < num.bitWidth {
+            num = num >> 1
+            index += 1
+        }
+        return index
+    }
+    /**
+     判断数字的第index位是不是1
+     - parameters:
+        - num: 数字
+        - index: 第index位（从右开始）
+     - Returns: 判断结果
+     */
+    private func IsBit1(_ num: Int, at index:Int) ->  Bool {
+        let num = num >> index
+        return num & 1 == 1
+    }
+}
+
+class UnitTests: XCTestCase {
+    var solution: Solution!
+    
+    override func setUp() {
+        super.setUp()
+        solution = Solution()
+    }
+
+    func testCase1() {
+        let nums = [2,4,3,6,3,2,5,5]
+        XCTAssertEqual(4, solution.FindNumsAppearOnce(nums).num1)
+        XCTAssertEqual(6, solution.FindNumsAppearOnce(nums).num2)
+    }
+    
+    func testCase2() {
+        let nums = [4,6]
+        XCTAssertEqual(4, solution.FindNumsAppearOnce(nums).num1)
+        XCTAssertEqual(6, solution.FindNumsAppearOnce(nums).num2)
+    }
+    
+    func testCase3() {
+        let nums = [4,6,1,1,1,1]
+        XCTAssertEqual(4, solution.FindNumsAppearOnce(nums).num1)
+        XCTAssertEqual(6, solution.FindNumsAppearOnce(nums).num2)
+    }
+}
+UnitTests.defaultTestSuite.run()
+
+
+
+
+
+
---
+++ 56_02_NumberAppearingOnce.playground/Contents.swift
@@ -0,0 +1,93 @@
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+//==================================================================
+
+// 面试题56（二）：数组中唯一只出现一次的数字
+// 题目：在一个数组中除了一个数字只出现一次之外，其他数字都出现了三次。请
+// 找出那个出现一次的数字。
+
+import Foundation
+import XCTest
+
+class Solution {
+    
+    /**
+    返回数组中只出现一次的数字
+     - parameters:
+        - nums: 数组
+     - Returns: 出现一次的数字
+     */
+    func FindNumberAppearingOnce(_ nums: [Int]) -> Int? {
+        guard nums.count > 0 else {
+            return nil
+        }
+        var bitSum = Array.init(repeating: 0, count: Int.bitWidth)
+        nums.forEach {
+            var bitMask = 1
+            for i in 0..<Int.bitWidth {
+                let bit = $0 & bitMask
+                bitSum[i] += bit == 0 ? 0 : 1
+                bitMask = bitMask << 1
+            }
+        }
+        //print(bitSum)
+        var result = 0
+        for i in stride(from: Int.bitWidth - 1, through: 0, by: -1) {
+            result = result << 1
+            result += bitSum[i] % 3
+        }
+        return result
+    }
+}
+
+class UnitTests: XCTestCase {
+    var solution: Solution!
+    
+    override func setUp() {
+        super.setUp()
+        solution = Solution()
+    }
+    func testCase1() {
+        let nums = [1,1,1,2,2,2,3]
+        XCTAssertEqual(3, solution.FindNumberAppearingOnce(nums))
+    }
+    func testCase2() {
+        let nums = [4, 3, 3, 2, 2, 2, 3]
+        XCTAssertEqual(4, solution.FindNumberAppearingOnce(nums))
+    }
+    func testCase3() {
+        let nums = [4,4,1,1,1,7,4]
+        XCTAssertEqual(7, solution.FindNumberAppearingOnce(nums))
+    }
+    func testCase4() {
+        let nums = [-10,214,214,214]
+        XCTAssertEqual(-10, solution.FindNumberAppearingOnce(nums))
+    }
+    func testCase5() {
+        let nums = [-209, 3467, -209, -209]
+        XCTAssertEqual(3467, solution.FindNumberAppearingOnce(nums))
+    }
+    func testCase6() {
+        let nums = [1024, -1025, 1024, -1025, 1024, -1025, 1023]
+        XCTAssertEqual(1023, solution.FindNumberAppearingOnce(nums))
+    }
+    func testCase7() {
+        let nums = [-1024, -1024, -1024, -1023]
+        XCTAssertEqual(-1023, solution.FindNumberAppearingOnce(nums))
+    }
+    func testCase8() {
+        let nums = [-23, 0, 214, -23, 214, -23, 214]
+        XCTAssertEqual(0, solution.FindNumberAppearingOnce(nums))
+    }
+    func testCase9() {
+        let nums = [0,3467,0,0,0,0,0,0]
+        XCTAssertEqual(3467, solution.FindNumberAppearingOnce(nums))
+    }
+}
+UnitTests.defaultTestSuite.run()
+
+
+
+
+
+
