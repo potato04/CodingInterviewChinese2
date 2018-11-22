c46077b5ad5abf7cf3930617db1886f3c9c82731
potato04 <shiww@outlook.com>
2018-11-22 22:58:24 +0800

    improve code style : 03-1

--- 03_01_DuplicationInArray.playground/Contents.swift
+++ 03_01_DuplicationInArray.playground/Contents.swift
@@ -1,7 +1,6 @@
 //==================================================================
 // 《剑指Offer——名企面试官精讲典型编程题》代码
 //==================================================================
-
 // 面试题3（一）：找出数组中重复的数字
 // 题目：在一个长度为n的数组里的所有数字都在0到n-1的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，
 // 也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。例如，如果输入长度为7的数组{2, 3, 1, 0, 2, 5, 3}，
@@ -15,11 +14,10 @@ class Solution {
      查找整数数组中任一重复的数字
      - parameters:
         - nums: 整数数组
-     - Returns: (重复数字的索引，值)
+     - Returns: Tuple(重复数字的索引，值)
      */
-    func duplicate(_ nums: [Int]) -> (Int, Int)? {
+    func duplicate(_ nums: [Int]) -> (index: Int, num: Int)? {
         var nums = nums
-        print(nums.count)
         for index in 0..<nums.count {
             while nums[index] != index {
                 if nums[index] == nums[nums[index]] {
@@ -34,7 +32,6 @@ class Solution {
     }
 }
 
-
 class UnitTests: XCTestCase {
     var solution: Solution!
     
@@ -45,18 +42,18 @@ class UnitTests: XCTestCase {
     ///重复数字是数组中最小的数字
     func testCase1() {
         let nums = [2, 1, 3, 1, 4]
-        XCTAssertEqual(solution.duplicate(nums)!.1, 1)
+        XCTAssertEqual(solution.duplicate(nums)!.num, 1)
     }
     ///重复数字是数组中最大的数字
     func testCase2() {
         let nums = [2, 4, 3, 1, 4]
-        XCTAssertEqual(solution.duplicate(nums)!.1, 4)
+        XCTAssertEqual(solution.duplicate(nums)!.num, 4)
     }
     ///数组中存在多个重复的数字
     func testCase3() {
         let nums = [2, 4, 2, 1, 4]
         let duplications = [2,4]
-        XCTAssertTrue(duplications.contains(solution.duplicate(nums)!.1))
+        XCTAssertTrue(duplications.contains(solution.duplicate(nums)!.num))
     }
     ///数组中不存在重复的数字
     func testCase4() {
@@ -71,6 +68,3 @@ class UnitTests: XCTestCase {
 }
 
 UnitTests.defaultTestSuite.run()
-
-
-
